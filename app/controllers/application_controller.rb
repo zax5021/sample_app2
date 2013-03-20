37944309f6c2992ef9759e03498f053d4c77f2c2
Zack Campbell <zacharyacampbell1@gmail.com>
2013-03-20 03:34:27 -0700

    Finish sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+
+  #to prevent CSRF attacks...force signout
+  def handle_unverified_request
+  	sign_out
+  	super
+  end
 end
