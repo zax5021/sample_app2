6f09e7986d587cf94e74249bb5a49d35fe6bd6d2
Zack Campbell <zacharyacampbell1@gmail.com>
2013-03-19 23:41:45 -0700

    Revert "Finish user signup"
    This reverts commit 9d5451337aa56e046e8d8db4c02f9f3d3bf943de.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,4 @@
 class UsersController < ApplicationController
-	def show
-		@user = User.find(params[:id])
-	end
-	
-	def new
-		@user = User.new
-	end
-
-	def create
-		@user = User.new(params[:user])
-		if @user.save
-			flash[:success] = "Welcome to the Sample App!"
-			redirect_to @user
-		else
-			render 'new'
-		end
-	end
+  def new
+  end
 end
